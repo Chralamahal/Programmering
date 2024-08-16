ca7720095a3376b37ef5ad2573fda0bf4a22da81
Chralamahal <chralamahal@gmail.com>
2024-08-16 12:11:17 +0200

    gonna sync now?

--- 01 web template/index.js
+++ 01 web template/index.js
@@ -12,29 +12,26 @@ function setup(){
 
 function shiftPage(num){
     console.log(num)
-    if(isNaN(num) || num > pages.length){
+    if(isNaN(num)   ||    num    >  pages.length ||  num  ==   0){
         console.log("return")
         return
-        
     }
     select("#page" + currentPage).removeClass("visible")
     currentPage = num
     select("#page" + currentPage).addClass("visible")
 }
 
-
 function keyPressed(){
     //console.log(key)
     if (key == "ArrowLeft"){
-        if (currentPage > 1){
+        if (currentPage >   1){
             shiftPage(currentPage - 1)
         }
     }
     if (key == "ArrowRight"){
-        if (currentPage < pages.length){
-            shiftPage(currentPage + 1)
+        if (currentPage <   pages.length){
+            shiftPage(currentPage   +   1)
         }
     }
-
     shiftPage(key)
 }
